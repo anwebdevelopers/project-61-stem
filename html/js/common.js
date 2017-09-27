98bae96363835f5b9e47341ce286b26f4a423103
anwebdevelopers <anwebdev78@gmail.com>
2017-09-27 19:41:13 +0300

    27.09.2017

--- html/js/common.js
+++ html/js/common.js
@@ -2,9 +2,9 @@ $(function() {
 
     'use strict';
 
-    /*******************************************************/
+    //*****************************************************//
     //FIRST SCREEN SLIDER
-    /*******************************************************/
+    //*****************************************************//
     var $headerBanner = $('.header__slide');
     if ($headerBanner.length > 1) {
         $headerBanner.wrapAll('<div class="header__slider owl-carousel"></div>');
@@ -23,9 +23,9 @@ $(function() {
     }
 
 
-    /*******************************************************/
+    //*****************************************************//
     //REVIEWS SLIDER
-    /*******************************************************/
+    //*****************************************************//
 
     $('.reviews__box').addClass('owl-carousel').owlCarousel({
         loop: true,
@@ -33,7 +33,7 @@ $(function() {
         navText: '',
         autoplay: true,
         autoplayTimeout: 5000,
-        smartSpeed: 600,
+        smartSpeed: 500,
         responsiveClass: true,
         responsive: {
             0: {
@@ -45,10 +45,69 @@ $(function() {
         }
     });
 
+    //*****************************************************//
+    //Product Gallery
+    //*****************************************************//
 
-    //*****************************************************
+    var images = Array();
+    $('.product__gallery').find('.product__gallery-item').each(function() {
+        images.push($(this).html());
+    }).end().addClass('owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        nav: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        smartSpeed: 500,
+        onInitialized: function() {
+            $('.owl-dot').each(function() {
+                $(this).html(images[$(this).index()]);
+            });
+        }
+    });
+
+    //*****************************************************//
+    //Product Description
+    //*****************************************************//
+
+    $('.product__description').each(function() {
+        var $this = $(this),
+            $productDescriptionText = $this.find('.product__description-text'),
+            height = $productDescriptionText.height();
+        if ( height >= 130 ) {
+            $productDescriptionText.css({'max-height':'12.8rem'}).after('<button class="product__description-button">Развернуть</button>');
+        }
+
+        $this.on('click', '.product__description-button', function() {
+            var $this = $(this);
+            if ( !$this.hasClass('active') ) {
+                $this.addClass('active').text('Свернуть')
+                .closest('.product__description').find($productDescriptionText).css({'max-height': height + 'px'})
+            } else {
+                $this.removeClass('active').text('Развернуть')
+                .closest('.product__description').find($productDescriptionText).css({'max-height': '12.8rem'});
+            }
+        });
+    });
+
+    //*****************************************************//
+    //Product Tabs
+    //*****************************************************//
+
+    $('.product__tabs').each(function() {
+        var $this = $(this);
+        $this.prepend('<div class="product__tabs-buttons"></div>')
+        .find('.product__tabs-item').each(function() {
+            $(this).find('.product__tabs-title').addClass( $(this).index() === 1 ? 'active' : '' ).appendTo('.product__tabs-buttons');
+        }).not(':first').hide();
+        $this.find('.product__tabs-buttons').on('click', '.product__tabs-title:not(.active)', function() {
+            $(this).addClass('active').siblings().removeClass('active').closest('.product__tabs').find('.product__tabs-item').slideUp(300).eq($(this).index()).slideDown(300);
+        });
+    });
+
+    //*****************************************************//
     // Google Map
-    //*****************************************************
+    //*****************************************************//
     if(typeof google === 'object' && typeof google.maps === 'object' && $('#map').length) {
         var markerPosition = new google.maps.LatLng(55.763319, 37.551117);
 
@@ -75,9 +134,9 @@ $(function() {
         google.maps.event.addDomListener(window, 'load', initialize);
     }
 
-    //******************************************************************//
+    //*****************************************************//
     //Chrome Smooth Scroll
-    //******************************************************************//
+    //*****************************************************//
     try {
         $.browserSelector();
         if ($('html').hasClass('chrome')) {
@@ -91,9 +150,9 @@ $(function() {
         event.preventDefault();
     });
 
-    /*******************************************************/
+    //*****************************************************//
     //Fullscreen Header for IE
-    /*******************************************************/
+    //*****************************************************//
 
     function fullscreen() {
         if( $('html').hasClass('ie') || $('html').hasClass('gecko') ) {
